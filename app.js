267cf35a8e515816bdd39a745fbbac23025ab785
Stephen Tai <stephentai@fundpark.com>
2022-05-18 14:03:47 +0800

    [20220518] Express js

--- app.js
+++ app.js
@@ -1,60 +1,109 @@
-const restify = require('restify');
-const config = require('./config/index');
-const app = restify.createServer();
-const rjwt = require('restify-jwt-community');
-const jwt = require('jsonwebtoken');
-const sa = require('./serverAction');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const config =require('./config/index')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const User = require('./models/user')
 
-app.use(restify.plugins.queryParser());
-app.use(restify.plugins.bodyParser());
+app.use(express.json());
 
-// app.use(rjwt({ secret: config.JWT_SECRET }).unless({
-//     path: ['/api/auth', '/api/registration']
-// }));
-
-// ========= Handle promise =========
-
-function catchErrors(callback) {
-    return async function errorHandler(req, res, next) {
-      try {
-        await callback(req, res, next);
-      } catch (err) {
-        if (!(err instanceof restify.errors.HttpError))
-          err = new restify.errors.InternalServerError(err);
-        next(err);
-      }
+app.post("/api/login", async (req, res, next) => {
+    let { email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    } catch {
+        return next(new Error('Error occured while query user'));
     }
-}
-
-app.on('InternalServer', function (req, res, err, next) {
-  let cause = err.cause()
-  if (cause && cause.name == 'ValidationError') {
-    err.statusCode = 400;
-    err.body.code = 'ValidationError';
-    err.body.message = cause.message;
-    err.body.details = cause.details;
-  } else {
-    log.error(err);
-    err.body = 'Something went wrong.';
-  }
-  next();
-})
-
-const database = mongoose.connection;
-database.on('error', (err) => {
-    console.log(err);
-})
-// import api from routes file
-database.once('open', () => {
-    require('./routes/user')(app)
-})
-
-app.listen(config.PORT, () => {
-    mongoose.connect(
-        config.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,   
+    if (!existingUser || existingUser.password != password) {
+        return next(new Error('No user found'));
     }
-    );
+    let token;
+    console.log(existingUser);
+
+    try {
+        token = jwt.sign(
+            { _id: existingUser._id, email: existingUser.email },
+            config.JWT_SECRET,
+            {expiresIn: "30m"}
+        );
+    } catch (err) {
+        return next(new Error('JWT sign error'))
+    }
+    // create response
+    res.status(201).json({
+        success: true,
+        data: {
+            userId: existingUser._id,
+            email: existingUser.email,
+            token: token,
+        }
+    });
+})
+
+app.post("/api/signup", async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const newUser = User({
+        username,
+        email,
+        password
+    });
+    try { 
+        await newUser.save();
+    } catch {
+        const err = new Error("Create user error ");
+        return next(err);
+    }
+    // Sign jwt token 
+    let token;
+    try {
+        token = jwt.sign(
+            {userId: newUser.id, email: newUser.email},
+            config.JWT_SECRET,
+            {expiresIn: '10m'}
+        );
+    } catch (err) {
+        const error = new Error('jwt sign error');
+        return next(error);
+    }
+    // create response
+    res.status(201).json({
+        success: true,
+        data: {
+            userId: newUser.id,
+            email: newUser.email,
+            token: token,
+        }
+    });
+});
+
+
+app.get('/api/accessResource', (req, res)=>{   
+    const token = req.headers.authorization.split(' ')[1];  
+    //Authorization: 'Bearer TOKEN'
+    if(!token)
+    {
+        res.status(200).json({success:false, message: "Error!Token was not provided."});
+    }
+    //Decoding the token
+    const decodedToken = jwt.verify(token, config.JWT_SECRET );
+    console.log(decodedToken);
+    res.status(200).json({
+        success:true,
+        data: {
+            userId:decodedToken.userId, 
+            email:decodedToken.email
+    }});    
+});
+
+mongoose.connect(
+    config.MONGODB_URI
+).then(
+    () => {
+        app.listen(config.PORT, () => {
+            console.log(`Example app listening on port ${config.PORT}`)
+        });
+}).catch(
+    (err) => {
+        console.log(`Error:  ${err}`)
 });
--- config/index.js
+++ config/index.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 8080,
