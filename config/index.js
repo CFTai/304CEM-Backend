7bfe745027f31f3733704e4dc82915eb506ac42e
Stephen Tai <stephentai@fundpark.com>
2022-06-09 18:35:25 +0800

    Update

--- config/index.js
+++ config/index.js
@@ -6,6 +6,6 @@ module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 8080,
     URL: process.env.BASE_URL || 'http://localhost:8080',
-    MONGODB_URI: 'mongodb://localhost:27017/304CEM-Development',
+    MONGODB_URI: 'mongodb://127.0.0.1:27017/304CEM-Development',
     JWT_SECRET: process.env.JWT_SECRET || 'supersecretkey',
 }
